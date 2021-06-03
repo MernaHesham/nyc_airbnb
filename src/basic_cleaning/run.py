cb2c51bdd7b78ae223fbdaeda3b86fa3bca28755
Merna Hesham <mernahesham97@yahoo.com>
2021-06-03 15:22:54 +0200

    Added Cleaning Data Step

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-An example of a step using MLflow and Weights & Biases
+Download from W&B the raw dataset and apply some basic data cleaning, exporting the result to a new artifact
 """
 import argparse
 import logging
@@ -13,34 +13,97 @@ logger = logging.getLogger()
 
 def go(args):
 
-    run = wandb.init(job_type="my_step")
+    run = wandb.init(project="nyc_airbnb", job_type="basic_cleaning")
     run.config.update(args)
 
     # Download input artifact. This will also log that this script is using this
     # particular version of the artifact
     # artifact_local_path = run.use_artifact(args.input_artifact).file()
 
-    ######################
-    # YOUR CODE HERE     #
-    ######################
+    logger.info(f"Fetching {args.input_artifact} from W&B...")
+    artifact_local_path = run.use_artifact(args.input_artifact).file()
+
+    logger.info("Reading with pandas")
+    df = pd.read_csv(artifact_local_path)
+
+    # Drop outliers
+    min_price = args.min_price
+    max_price = args.max_price
+    idx = df['price'].between(min_price, max_price)
+    df = df[idx]
+
+    # Convert last_review to datetime
+    df['last_review'] = pd.to_datetime(df['last_review'])
+
+    # Fill the null dates with an old date
+    df['last_review'].fillna(pd.to_datetime("2010-01-01"), inplace=True)
+
+    # If the reviews_per_month is nan it means that there is no review
+    df['reviews_per_month'].fillna(0, inplace=True)
+
+    # We can fill the names with a short string.
+    # DO NOT use empty strings here
+    df['name'].fillna('-', inplace=True)
+    df['host_name'].fillna('-', inplace=True)
+
+    df.to_csv("clean_sample.csv", index=False)
+
+    artifact = wandb.Artifact(
+     args.output_name,
+     type=args.output_type,
+     description=args.output_description,
+    )
+    artifact.add_file("clean_sample.csv")
+    run.log_artifact(artifact)
+
+
+
 
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="My step")
+    parser = argparse.ArgumentParser(description="A very basic data cleaning")
 
 
     parser.add_argument(
-        "--parameter1", 
-        type=str,
-        help="p 1",
+        "--input_artifact", 
+        type=## INSERT TYPE HERE: str, float or int,
+        help=## INSERT DESCRIPTION HERE,
         required=True
     )
 
     parser.add_argument(
-        "--parameter2", 
-        type=str,
-        help="p 2",
+        "--output_name", 
+        type=## INSERT TYPE HERE: str, float or int,
+        help=## INSERT DESCRIPTION HERE,
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_type", 
+        type=## INSERT TYPE HERE: str, float or int,
+        help=## INSERT DESCRIPTION HERE,
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_description", 
+        type=## INSERT TYPE HERE: str, float or int,
+        help=## INSERT DESCRIPTION HERE,
+        required=True
+    )
+
+    parser.add_argument(
+        "--min_price", 
+        type=## INSERT TYPE HERE: str, float or int,
+        help=## INSERT DESCRIPTION HERE,
+        required=True
+    )
+
+    parser.add_argument(
+        "--max_price", 
+        type=## INSERT TYPE HERE: str, float or int,
+        help=## INSERT DESCRIPTION HERE,
         required=True
     )
 
